dd826644ce73ca0d617bdf8cf2b59b53906458f5
wrexlua <ahmetkaanakhan1980@gmail.com>
2026-09-17 17:21:00 +0300

    Create AZU_CRASH.lua

---
+++ functions/AZU_CRASH.lua
@@ -0,0 +1,18 @@
+local function doChecks()
+  if typeof(getgenv) ~= "function" or getfenv(getgenv) ~= getfenv(0) then
+    return "91"
+  end
+end
+
+
+getgenv().AZU_CRASH() = function(mode)
+  if mode == "PLR" then
+    while true do end
+  elseif mode == "VM" then
+    task.spawn(function() while true do end; end)
+
+  else
+    return "24"
+  end
+  end
+end
